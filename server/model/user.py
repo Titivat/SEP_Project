09e166eb756801b58729a9c58f8b1f39bf5be87d
61090001 <61090001@kmitl.ac.th>
2020-05-02 15:48:21 +0700

    Add User model
    Add User class for relational mapping with bcrypt password
    hash validation.

---
+++ server/model/user.py
@@ -0,0 +1,21 @@
+import datetime
+from .. import db, bcrypt
+from sqlalchemy.ext.hybrid import hybrid_property
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    username = db.Column(db.String, primary_key=True)
+    _password = db.Column(db.Binary)
+    created_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    @hybrid_property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, plaintext):
+        self._password = bcrypt.generate_password_hash(plaintext, 10)
+
+    def verify_password(self, plaintext):
+        return bcrypt.check_password_hash(self._password, plaintext)
