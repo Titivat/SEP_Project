d4e44ddab874a1e2f02449f4483d8f85090e6013
61090001 <61090001@kmitl.ac.th>
2020-05-27 11:37:01 +0700

    Add User and Document services

--- server/loop.py
+++ server/loop.py
@@ -26,6 +26,7 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 
     def __init__(self, request, client_address, server):
         super().__init__(request, client_address, server)
+        self.user = None
     
     def setup(self):
         super().setup()
@@ -44,29 +45,80 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 
             for o in unpacker:
                 if "action" not in o:
+                    # No action, so skip it
                     continue
+
                 if o["action"] == "register" and "username" in o and "password" in o:
+                    # Register user
                     username = o["username"]
                     password = o["password"]
+                    if username.strip() == "" or password.strip() == "":
+                        self.request.sendall(msgpack.packb({"success": False, "ctx": "register", "err": "username or password cannot be empty"}))
+                        continue
                     user = User(username=username, password=password)
                     session.add(user)
-                    self.request.sendall(msgpack.packb({"success": True}))
+                    try:
+                        session.commit()
+                        self.request.sendall(msgpack.packb({"success": True, "ctx": "register"}))
+                    except Exception as e:
+                        session.rollback()
+                        self.request.sendall(msgpack.packb({"success": False, "ctx": "register", "err": str(e)}))
+
                 elif o["action"] == "login" and "username" in o and "password" in o:
+                    # Login
                     username = o["username"]
                     password = o["password"]
                     user = session.query(User).filter(User.username==username).first()
                     if not user:
                         logging.error("user not found")
-                        self.request.sendall(msgpack.packb({"success": False, "err": "user not found"}))
+                        self.request.sendall(msgpack.packb({"success": False, "ctx": "login", "err": "user not found"}))
                         continue
                     if not user.verify_password(password):
                         logging.error("wrong password")
-                        self.request.sendall(msgpack.packb({"success": False, "err": "wrong password"}))
+                        self.request.sendall(msgpack.packb({"success": False, "ctx": "login", "err": "wrong password"}))
                         continue
                     logging.info("correct password")
-                    self.request.sendall(msgpack.packb({"success": True}))
+                    self.request.sendall(msgpack.packb({"success": True, "ctx": "login"}))
+                    self.user = user
 
-            session.commit()
+                if not self.user:
+                    # Not login just pass
+                    self.request.sendall(msgpack.packb({"success": False, "err": "user not logged in"}))
+                    continue
+
+                if o["action"] == "logout":
+                    # Logout user
+                    self.user = None
+                    self.request.sendall(msgpack.packb({"success": True, "ctx": "logout"}))
+                
+                elif o["action"] == "users":
+                    # Retrieve users
+                    users = []
+                    for u in session.query(User).filter(User.username!=self.user.username):
+                        users.append(u.username)
+                    self.request.sendall(msgpack.packb({"success": True, "ctx": "users", "users": users}))
+                
+                elif o["action"] == "create" and "name" in o:
+                    # Create document
+                    name = o["name"]
+                    if name.strip() == "":
+                        self.request.sendall(msgpack.packb({"success": False, "ctx": "create", "err": "document name cannot be empty"}))
+                        continue
+                    doc = Document(name=name, owner=self.user)
+                    session.add(doc)
+                    try:
+                        session.commit()
+                        self.request.sendall(msgpack.packb({"success": True, "ctx": "create"}))
+                    except Exception as e:
+                        session.rollback()
+                        self.request.sendall(msgpack.packb({"success": False, "ctx": "create", "err": str(e)}))
+                
+                elif o["action"] == "documents":
+                    # Retrieve documents for users
+                    docs = []
+                    for d in session.query(Document).filter(Document.owner==self.user).all():
+                        docs.append({"id": d.id, "name": d.name})
+                    self.request.sendall(msgpack.packb({"success": True, "ctx": "documents", "documents": docs}))
     
     def finish(self):
         super().finish()
